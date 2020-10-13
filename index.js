9ad6b578d5361935b5a8bc57cc8ff4b25574996b
Chris Parnin <chris.parnin@gmail.com>
2020-10-13 19:01:31 -0400

    initial

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3030
+
+app.get('/', (req, res) => {
+
+    let referrer = req.get('Referrer');
+
+    if( referrer )
+    {
+        referrer = referrer.replace("https://", "docable://")
+                            .replace("http://", "docable://")
+        res.redirect(referrer);
+    }
+})
+
+app.listen(port, () => {
+  console.log(`Badge service listening on http://localhost:${port}`)
+})
